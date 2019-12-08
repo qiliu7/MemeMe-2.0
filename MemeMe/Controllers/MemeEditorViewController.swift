0a37e92b51118d4606706e77ad87e3a14a34ea8c
Qi <liuqi@alphae.cn>
2019-12-07 21:09:51 -0600

    Add cancelEditing func to the cancelButton.

--- MemeMe/Controllers/MemeEditorViewController.swift
+++ MemeMe/Controllers/MemeEditorViewController.swift
@@ -85,6 +85,10 @@ class MemeEditorViewController: UIViewController {
     present(activityController, animated: true, completion: nil)
   }
   
+  @IBAction private func cancelEditing() {
+    dismiss(animated: true, completion: nil)
+  }
+  
   private func subscribeToKeyboardNotifications() {
     NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
     NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)

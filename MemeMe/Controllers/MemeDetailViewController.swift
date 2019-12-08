8ff47b3a45ed9f7177b4a898d9ade332af746324
Qi <liuqi@alphae.cn>
2019-12-07 20:43:53 -0600

    Add MemeDetailViewController

---
+++ MemeMe/Controllers/MemeDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MemeDetailViewController.swift
+//  MemeMe
+//
+//  Created by Kappa on 2019/11/26.
+//  Copyright © 2019 qi. All rights reserved.
+//
+
+import UIKit
+
+class MemeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
